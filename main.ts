d9c6b024611a991721013054ebb4ccdf552f32b0
Nore09 <124691842+Nore09@users.noreply.github.com>
2023-02-14 15:05:46 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,16 @@
+let getal = 0
+basic.forever(function () {
+    if (getal < 12) {
+        basic.showNumber(0)
+        basic.showNumber(3)
+        basic.showNumber(6)
+        basic.showNumber(9)
+        basic.showNumber(12)
+    } else {
+        getal += 0
+        getal += 3
+        getal += 6
+        getal += 9
+        getal += 12
+    }
+})
